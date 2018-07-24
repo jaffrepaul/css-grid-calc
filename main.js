04d710395f5c665316bb7f7cb7cf89faf4299c9d
paul jaffre <paul.jaffre@makerbot.com>
2018-07-24 18:14:26 -0400

    added logic for event handlers only on buttons. WIP to get nums to display

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const buttons = document.getElementsByClassName('calc-button');
 const calculator = Array.from(document.getElementsByClassName('calculator-buttons'));
+const display = document.getElementById('display');
 
 calculator.forEach(button =>
     addEventListener('click', (e) => {
-        console.log(e.target);
+        if (e.target.nodeName === 'BUTTON') {
+            // console.log(e.target.value);
+            e.target.value += display.innerHTML;
+        }
     })
 );
+
+function displayInfo(e) {
+    e.target.value += display;
+}
