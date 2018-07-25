871364826d18167d568c1a970ebeda32f63093a9
paul jaffre <paul.jaffre@makerbot.com>
2018-07-25 00:42:07 -0400

    basic calc function working in display window

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-const buttons = document.getElementsByClassName('calc-button');
 const calculator = Array.from(document.getElementsByClassName('calculator-buttons'));
 const display = document.getElementById('display');
+console.log(display);
 
 calculator.forEach(button =>
-    addEventListener('click', (e) => {
+    addEventListener('click', e => {
         if (e.target.nodeName === 'BUTTON') {
-            // console.log(e.target.value);
-            e.target.value += display.innerHTML;
+            let val = e.target.innerHTML;
+
+            if (val === '=') {
+                display.innerHTML = eval(display.innerHTML);
+            } else {
+                display.innerHTML += val;
+            }
         }
     })
 );
 
-function displayInfo(e) {
-    e.target.value += display;
-}
+// function displayInfo(e) {
+//     e.target.value += display;
+// }
